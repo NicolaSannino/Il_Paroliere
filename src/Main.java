bbe0d555da99392fe3c05c3b26d4baf1ada12240
AndreaComolli <andre.comolli@edu.cobianchi.it>
2023-01-17 12:05:37 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao raga, sium lo fkomo!");
+        System.out.print("messi messi messi, ancara messi");
     }
+    
 }
