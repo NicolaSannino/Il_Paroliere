bc97c5e1e4919375511618c1511f593a61924034
AlbertoTarabella <alberto.tarabella@edu.cobianchi.it>
2023-01-17 12:19:46 +0100

    palo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao raga, sium lo fkomo!");
         System.out.print("messi messi messi, ancara messi");
+        System.out.print("palo palo palo");
     }
     
 }
